83e29afe045c1ec2f45aa1afcca52f2a2031a7d0
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2023-08-28 11:22:52 -0300

    Update mediapipe_test.py

--- Testing/mediapipe_test.py
+++ Testing/mediapipe_test.py
@@ -6,9 +6,12 @@ mp_drawing = mp.solutions.drawing_utils
 drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 face_mesh = mp_face_mes.FaceMesh(max_num_faces=1, refine_landmarks=True, min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
-image = cv2.imread('eunavida.jpg')
-image = cv2.resize(image, (0, 0), fx=2, fy=2)
+image = cv2.imread('Testing/eunavida.jpg')
+image = cv2.resize(image, (0, 0), fx=1, fy=1)
 empty = np.zeros(image.shape, dtype=np.uint8)
+
+print(f"Total edges: {len(mp_face_mes.FACEMESH_TESSELATION)}")
+print(f"Edges of lips, eyes, eyebrows, nose and contour: {len(mp_face_mes.FACEMESH_LIPS) + len(mp_face_mes.FACEMESH_RIGHT_EYE) + len(mp_face_mes.FACEMESH_LEFT_EYE) + len(mp_face_mes.FACEMESH_RIGHT_EYEBROW) + len(mp_face_mes.FACEMESH_LEFT_EYEBROW) + len(mp_face_mes.FACEMESH_FACE_OVAL) + len(mp_face_mes.FACEMESH_NOSE)}")
 
 results = face_mesh.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
