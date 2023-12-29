4201cb0a50750ac70a0d06de2d5887b1ba3f35b7
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2023-12-29 00:41:02 -0300

    Augment using albumentations lib

--- augment.py
+++ augment.py
@@ -1,32 +1,28 @@
+import albumentations
 import cv2
 import numpy as np
 import os
 
-def stretch_image(image_path, output_path):
-    axis = np.random.choice(['x', 'y'])
-    if axis == 'x':
-        stretch_factor_x = np.random.uniform(1.2, 1.5)
-        stretch_factor_y = 1.0
-    else:
-        stretch_factor_x = 1.0
-        stretch_factor_y = np.random.uniform(1.2, 1.5)
-    original_image = cv2.imread(image_path)
-    height, width = original_image.shape[:2]
-    new_height = int(height * stretch_factor_y)
-    new_width = int(width * stretch_factor_x)
-    stretched_image = cv2.resize(original_image, (new_width, new_height))
-    cv2.imwrite(output_path, stretched_image)
+folder = 'face_surprised'
+limit = 3717
 
-folder = 'face_sad'
-limit = 500
+transform_pipeline = albumentations.Compose([
+    albumentations.GridDistortion(p=0.5, num_steps=5, distort_limit=0.2),
+    albumentations.OpticalDistortion(p=0.5, distort_limit=0.2, shift_limit=0.05),
+    albumentations.HorizontalFlip(p=0.5),
+])
+
 images = os.listdir(folder)
 np.random.shuffle(images)
 images = images[:limit]
 for image in images:
     image_path = os.path.join(folder, image)
-    output_path = os.path.join(folder, f"stretched_{image}")
+    output_path = os.path.join(folder, f"augmented_{image}")
     try:
-        stretch_image(image_path, output_path)
+        image = cv2.imread(image_path)
+        transformed = transform_pipeline(image=image)
+        transformed_image = transformed["image"]
+        cv2.imwrite(output_path, transformed_image)
     except Exception as e:
         print(e)
         continue
